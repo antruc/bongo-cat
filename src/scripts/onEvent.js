7de087a9b063f5e33d6673bdd39e94cbb61d9d2b
Antruc <admin@antruc.dev>
2025-05-23 12:27:35 -0700

    Add comments

--- src/scripts/onEvent.js
+++ src/scripts/onEvent.js
@@ -1,8 +1,9 @@
 export default function onEvent(name, listener) {
+  // Add more than one EventListener
   if (/\s/.test(name)) {
     let names = name.split(' ')
     names.forEach((i) => {
-      document.addEventListener(i, listener, { passive: false })
+      document.addEventListener(i, listener, { passive: false }) // Stop logging
     })
   } else {
     document.addEventListener(name, listener, false)
